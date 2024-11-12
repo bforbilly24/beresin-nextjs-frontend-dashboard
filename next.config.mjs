95decacfaa73e46997f3cc228917b5c2646af644
Halim <halimputra0701@gmail.com>
2024-11-13 00:50:09 +0700

    core(configuration): update configuration

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+// next.config.mjs
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			// Add your remote image patterns here if needed
+		],
+	},
+	transpilePackages: ['geist'],
+};
+
+export default nextConfig;
