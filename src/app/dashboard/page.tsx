b660b3cced42cbefbcaa5fb94940e012935feb9c
Halim <halimputra0701@gmail.com>
2024-11-13 15:54:59 +0700

    refactor(page): add session for page dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,18 @@
-import { auth } from '@/auth';
+// src/app/dashboard/page.tsx
+import getServerSession from 'next-auth';
 import { redirect } from 'next/navigation';
+import authConfig from '@/utils/auth.config';
 
 export default async function Dashboard() {
-  const session = await auth();
+	const session = await getServerSession(authConfig);
 
-  if (!session?.user) {
-    return redirect('/');
-  } else {
-    redirect('/dashboard/overview');
-  }
+	// If no session, redirect to login
+	if (!session) {
+		redirect('/');
+		return null;
+	}
+
+	// Redirect to overview if session exists
+	redirect('/dashboard/overview');
+	return null;
 }
