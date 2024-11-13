2732aaf69952ff56ac4ae53aea56823ad04da163
Halim <halimputra0701@gmail.com>
2024-11-13 15:23:39 +0700

    refactor(route): update configuration route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,7 @@
-import { handlers } from '@/auth';
-export const { GET, POST } = handlers;
+// src/app/api/auth/[...nextauth]/route.ts
+import authConfig from '@/utils/auth.config';
+import NextAuth from 'next-auth';
+
+const handler = NextAuth(authConfig);
+
+export { handler as GET, handler as POST };
