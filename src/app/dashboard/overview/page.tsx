b83aaf5e3a303f544b335fe45252af0d42860e6e
Halim <halimputra0701@gmail.com>
2024-11-13 15:55:07 +0700

    refactor(page): add session for page dashboard/overview

--- src/app/dashboard/overview/page.tsx
+++ src/app/dashboard/overview/page.tsx
@@ -1,9 +1,23 @@
+// src/app/dashboard/overview/page.tsx
+import { Metadata } from 'next';
+import getServerSession from 'next-auth';
+import { redirect } from 'next/navigation';
+import authConfig from '@/utils/auth.config';
 import OverViewPage from './_components/overview';
 
-export const metadata = {
-  title: 'Dashboard : Overview'
+export const metadata: Metadata = {
+	title: 'Dashboard : Overview',
 };
 
-export default function page() {
-  return <OverViewPage />;
+export default async function Page() {
+	const session = await getServerSession(authConfig);
+
+	// Redirect to login if there is no session (user is not authenticated)
+	if (!session) {
+		redirect('/');
+		return null;
+	}
+
+	// Render the overview p        age if the user is authenticated
+	return <OverViewPage />;
 }
