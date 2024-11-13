f0b4e086898440378bfd95c01b0b78e4a5e70e21
Halim <halimputra0701@gmail.com>
2024-11-13 16:03:10 +0700

    feat(type): add data user

---
+++ src/types/data-user.d.ts
@@ -0,0 +1,12 @@
+// src/types/data-user.ts
+
+export type DataUser = {
+	id: number;
+	created_at: string;
+	updated_at: string;
+	username: string;
+	name: string;
+	email: string;
+	phone: string;
+	role: string;
+};
