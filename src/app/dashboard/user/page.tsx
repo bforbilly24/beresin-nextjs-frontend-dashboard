b574ec1146b6b882cc9e99f0c038fb9fa5533506
Halim <halimputra0701@gmail.com>
2024-11-13 16:10:36 +0700

    refactor(page): add props users

--- src/app/dashboard/user/page.tsx
+++ src/app/dashboard/user/page.tsx
@@ -1,10 +1,14 @@
+// src/app/dashboard/user/page.tsx
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 import { SearchParams } from 'nuqs/parsers';
 import React from 'react';
+import { getUsers } from '@/actions/user/get-user';
 import { searchParamsCache } from '@/lib/searchparams';
 import authConfig from '@/utils/auth.config';
 import UserListingPage from './_components/user-listing-page';
+
+// Import the getUsers function
 
 type pageProps = {
 	searchParams: SearchParams;
@@ -21,8 +25,12 @@ export default async function Page({ searchParams }: pageProps) {
 		redirect('/');
 		return null;
 	}
+
+	// Fetch users using the session token
+	const users = await getUsers(session.accessToken || '');
+
 	// Allow nested RSCs to access the search params (in a type-safe way)
 	searchParamsCache.parse(searchParams);
 
-	return <UserListingPage />;
+	return <UserListingPage users={users} />;
 }
