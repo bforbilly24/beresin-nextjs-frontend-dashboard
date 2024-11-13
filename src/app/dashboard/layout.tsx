8dd001702f6cb8f285fa513aac3ba50fd8ecf250
Halim <halimputra0701@gmail.com>
2024-11-13 15:27:06 +0700

    refactor(layout): update content layout of dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,19 +1,15 @@
-import AppSidebar from '@/components/layout/app-sidebar';
 import type { Metadata } from 'next';
+import AppSidebar from '@/components/layout/app-sidebar';
 
 export const metadata: Metadata = {
-  title: 'Next Shadcn Dashboard Starter',
-  description: 'Basic dashboard with Next.js and Shadcn'
+	title: 'BeresIn Dashboard',
+	description: 'Basic dashboard with Next.js and Shadcn',
 };
 
-export default function DashboardLayout({
-  children
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <>
-      <AppSidebar>{children}</AppSidebar>
-    </>
-  );
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<>
+			<AppSidebar>{children}</AppSidebar>
+		</>
+	);
 }
