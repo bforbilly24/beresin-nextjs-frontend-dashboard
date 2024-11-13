e64a0bb0a6f9cfcc5e6e6c8577fd47917e0974ba
Halim <halimputra0701@gmail.com>
2024-11-13 16:03:30 +0700

    refactor(type): add next auth type as need

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,13 +1,33 @@
-import NextAuth, { DefaultSession } from 'next-auth';
+// src/types/next-auth.d.ts
+import { DefaultSession } from 'next-auth';
 
 declare module 'next-auth' {
-  type UserSession = DefaultSession['user'];
-  interface Session {
-    user: UserSession;
-  }
+	interface Session extends DefaultSession {
+		accessToken?: string;
+		user: {
+			id: string;
+			email: string;
+			role: string;
+			username?: string;
+			name?: string; 
+		} & DefaultSession['user'];
+	}
 
-  interface CredentialsInputs {
-    email: string;
-    password: string;
-  }
+	interface User {
+		id: string;
+		email: string;
+		username: string;
+		role: string;
+		name?: string;
+		accessToken?: string;
+	}
+
+	interface JWT {
+		id: string;
+		email: string;
+		username: string;
+		role: string;
+		name?: string;
+		accessToken?: string;
+	}
 }
