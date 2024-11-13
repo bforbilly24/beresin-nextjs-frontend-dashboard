60b16425cf02cc33e0823e2cc767151e5ef3ba75
Halim <halimputra0701@gmail.com>
2024-11-13 15:55:22 +0700

    refactor(page): add session for page dashboard/product

--- src/app/dashboard/product/page.tsx
+++ src/app/dashboard/product/page.tsx
@@ -1,3 +1,10 @@
+import { Plus } from 'lucide-react';
+import { Metadata } from 'next';
+import { getServerSession } from 'next-auth';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import { SearchParams } from 'nuqs/parsers';
+import { Suspense } from 'react';
 import PageContainer from '@/components/layout/page-container';
 import { buttonVariants } from '@/components/ui/button';
 import { Heading } from '@/components/ui/heading';
@@ -5,52 +12,45 @@ import { Separator } from '@/components/ui/separator';
 import { DataTableSkeleton } from '@/components/ui/table/data-table-skeleton';
 import { searchParamsCache, serialize } from '@/lib/searchparams';
 import { cn } from '@/lib/utils';
-import { Plus } from 'lucide-react';
-import Link from 'next/link';
-import { SearchParams } from 'nuqs/parsers';
-import { Suspense } from 'react';
+import authConfig from '@/utils/auth.config';
 import ProductListingPage from './_components/product-listing';
 import ProductTableAction from './_components/product-tables/product-table-action';
 
-export const metadata = {
-  title: 'Dashboard: Products'
+export const metadata: Metadata = {
+	title: 'Dashboard: Products',
 };
 
 type pageProps = {
-  searchParams: SearchParams;
+	searchParams: SearchParams;
 };
 
 export default async function Page({ searchParams }: pageProps) {
-  // Allow nested RSCs to access the search params (in a type-safe way)
-  searchParamsCache.parse(searchParams);
+	const session = await getServerSession(authConfig);
 
-  // This key is used for invoke suspense if any of the search params changed (used for filters).
-  const key = serialize({ ...searchParams });
+	if (!session) {
+		redirect('/');
+		return null;
+	}
 
-  return (
-    <PageContainer>
-      <div className="space-y-4">
-        <div className="flex items-start justify-between">
-          <Heading
-            title="Products"
-            description="Manage products (Server side table functionalities.)"
-          />
-          <Link
-            href="/dashboard/product/new"
-            className={cn(buttonVariants(), 'text-xs md:text-sm')}
-          >
-            <Plus className="mr-2 h-4 w-4" /> Add New
-          </Link>
-        </div>
-        <Separator />
-        <ProductTableAction />
-        <Suspense
-          key={key}
-          fallback={<DataTableSkeleton columnCount={5} rowCount={10} />}
-        >
-          <ProductListingPage />
-        </Suspense>
-      </div>
-    </PageContainer>
-  );
+	searchParamsCache.parse(searchParams);
+
+	const key = serialize({ ...searchParams });
+
+	return (
+		<PageContainer>
+			<div className='space-y-4'>
+				<div className='flex items-start justify-between'>
+					<Heading title='Products' description='Manage products (Server side table functionalities.)' />
+					<Link href='/dashboard/product/new' className={cn(buttonVariants(), 'text-xs md:text-sm')}>
+						<Plus className='mr-2 h-4 w-4' /> Add New
+					</Link>
+				</div>
+				<Separator />
+				<ProductTableAction />
+				<Suspense key={key} fallback={<DataTableSkeleton columnCount={5} rowCount={10} />}>
+					<ProductListingPage />
+				</Suspense>
+			</div>
+		</PageContainer>
+	);
 }
