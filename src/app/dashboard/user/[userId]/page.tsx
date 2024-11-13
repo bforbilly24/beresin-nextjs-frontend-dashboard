9dca8f6191778125fed3c35f15956bf28c2e52d8
Halim <halimputra0701@gmail.com>
2024-11-13 15:57:18 +0700

    style(page): update indentation tab

---
+++ src/app/dashboard/user/[userId]/page.tsx
@@ -0,0 +1,9 @@
+import UserViewPage from '../_components/user-view-page';
+
+export const metadata = {
+  title: 'Dashboard : User View'
+};
+
+export default function Page() {
+  return <UserViewPage />;
+}
