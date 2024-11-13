edbada88577cb2a83909ae2e942a49c354e4c809
Halim <halimputra0701@gmail.com>
2024-11-13 15:22:36 +0700

    refactor(auth): add session for signin page

--- src/app/(auth)/(signin)/page.tsx
+++ src/app/(auth)/(signin)/page.tsx
@@ -1,11 +1,27 @@
+// src/app/(auth)/(signin)/page.tsx
 import { Metadata } from 'next';
+import { getServerSession } from 'next-auth/next';
+import { redirect } from 'next/navigation';
 import SignInViewPage from '../_components/sigin-view';
+import authConfig from '@/utils/auth.config';
+
+// Correct import for getServerSession
 
 export const metadata: Metadata = {
-  title: 'Authentication | Sign In',
-  description: 'Sign In page for authentication.'
+	title: 'Sign In',
+	description: 'Sign In page for authentication.',
 };
 
-export default function Page() {
-  return <SignInViewPage />;
+export default async function Page() {
+	// Check for an existing session
+	const session = await getServerSession(authConfig);
+
+	// If a session exists, redirect to dashboard
+	if (session) {
+		redirect('/dashboard');
+		return null;
+	}
+
+	// If no session, render the sign-in page
+	return <SignInViewPage />;
 }
