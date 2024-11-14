cf9178d3af83eb59cc86ee22b0457afdeffa2a01
Halim <halimputra0701@gmail.com>
2024-11-14 13:00:09 +0700

    refactor(component): update form for method post and method put

--- src/app/dashboard/user/_components/user-form.tsx
+++ src/app/dashboard/user/_components/user-form.tsx
@@ -1,65 +1,81 @@
+// src/app/dashboard/user/_components/UserForm.tsx
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import * as React from 'react';
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
 import * as z from 'zod';
-import { deleteUser } from '@/actions/user/delete-user';
+import { getUsers } from '@/actions/user/get-user';
 import { postUser } from '@/actions/user/post-user';
 import { putUser } from '@/actions/user/put-user';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 
-// Define schemas for different actions
+// src/app/dashboard/user/_components/UserForm.tsx
+
+// Define schema for POST requests
 const postSchema = z.object({
-	id: z.number().optional(),
-	username: z.string().min(1, 'Username is required for creating a user.'),
+	username: z.string().min(1, 'Username is required.'),
 	name: z.string().min(2, 'Name must be at least 2 characters.'),
 	email: z.string().email('Please enter a valid email address.'),
-	phone: z.string().optional().default(''), // Ensure phone defaults to empty string
-	role: z.string(),
+	phone: z.string().min(10, 'Phone number must be valid.'),
 	password: z.string().min(6, 'Password must be at least 6 characters.'),
+	role: z.string(),
 });
 
-const putDeleteSchema = z.object({
-	id: z.number().nonnegative('ID is required for updating or deleting a user.'),
+// Define schema for PUT requests
+const putSchema = z.object({
+	id: z.number().nonnegative(),
 	username: z.string().optional(),
 	name: z.string().optional(),
 	email: z.string().optional(),
-	phone: z.string().optional().default(''), // Ensure phone defaults to empty string
+	phone: z.string().optional(),
 	role: z.string().optional(),
-	password: z.string().optional(),
 });
 
-// Type for the form data
-type FormData = z.infer<typeof postSchema> & z.infer<typeof putDeleteSchema>;
+type FormData = z.infer<typeof postSchema> & z.infer<typeof putSchema>;
 
-export default function UserForm() {
-	const { data: session } = useSession(); // Use NextAuth session in the client
-	const [action, setAction] = React.useState<'POST' | 'PUT' | 'DELETE'>('POST');
+interface UserFormProps {
+	session: any;
+	actionType: 'POST' | 'PUT';
+	userId?: string;
+}
+
+export default function UserForm({ session, actionType, userId }: UserFormProps) {
 	const router = useRouter();
 
 	const form = useForm<FormData>({
-		resolver: zodResolver(action === 'POST' ? postSchema : putDeleteSchema),
+		resolver: zodResolver(actionType === 'POST' ? postSchema : putSchema),
 		defaultValues: {
-			id: undefined,
+			id: userId ? parseInt(userId, 10) : undefined,
 			username: '',
 			name: '',
 			email: '',
-			phone: '', // Set phone to an empty string
-			role: '',
+			phone: '',
 			password: '',
+			role: '',
 		},
 	});
 
-	const onSubmit = async (values: FormData) => {
-		const token = session?.accessToken; // Get the token from session
+	// Fetch user data if in edit (PUT) mode
+	useEffect(() => {
+		const fetchUser = async () => {
+			if (actionType === 'PUT' && session?.accessToken && userId) {
+				const user = await getUsers(session.accessToken, parseInt(userId, 10));
+				if (user && !Array.isArray(user)) {
+					form.reset(user);
+				}
+			}
+		};
+		fetchUser();
+	}, [session, userId, actionType, form]);
 
+	const onSubmit = async (values: FormData) => {
+		const token = session?.accessToken;
 		if (!token) {
 			console.error('User is not authenticated.');
 			return;
@@ -67,56 +83,31 @@ export default function UserForm() {
 
 		try {
 			let response;
-			if (action === 'POST') {
+			if (actionType === 'POST') {
 				response = await postUser(token, values);
-				if (response && response.status === 'success') {
-					router.push('/dashboard/user'); // Redirect after successful POST
-				}
-			} else if (action === 'PUT' && values.id) {
+			} else if (actionType === 'PUT' && values.id) {
 				response = await putUser(token, values.id, values);
-				if (response && response.status === 'success') {
-					router.push('/dashboard/user'); // Redirect after successful PUT
-				}
-			} else if (action === 'DELETE' && values.id) {
-				response = await deleteUser(token, values.id);
-				if (response && response.message === 'User deleted successfully') {
-					router.push('/dashboard/user'); // Redirect after successful DELETE
-				}
+			}
+
+			if (response && response.status === 'success') {
+				toast.success(`User ${actionType === 'POST' ? 'created' : 'updated'} successfully`);
+				router.push('/dashboard/user');
 			}
 		} catch (error) {
-			console.error('Error performing action:', error);
+			console.error(`Error ${actionType === 'POST' ? 'creating' : 'updating'} user:`, error);
+			toast.error('An error occurred. Please try again.');
 		}
 	};
 
 	return (
 		<Card className='mx-auto w-full'>
 			<CardHeader>
-				<CardTitle className='text-left text-2xl font-bold'>User Information - {action} Action</CardTitle>
+				<CardTitle className='text-left text-2xl font-bold'>{actionType === 'POST' ? 'Add New User' : 'Edit User'}</CardTitle>
 			</CardHeader>
 			<CardContent>
 				<Form {...form}>
 					<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4 md:space-y-8'>
-						{/* Select Action */}
-						<div className='space-y-4'>
-							<FormLabel>Action</FormLabel>
-							<RadioGroup className='flex space-x-8' value={action} onValueChange={(value) => setAction(value as 'POST' | 'PUT' | 'DELETE')}>
-								<FormItem className='flex space-x-4 items-center justify-center'>
-									<RadioGroupItem value='POST' />
-									<FormLabel className='leading-none !mt-0'>Post</FormLabel>
-								</FormItem>
-								<FormItem className='flex space-x-4 items-center justify-center'>
-									<RadioGroupItem value='PUT' />
-									<FormLabel className='leading-none !mt-0'>Put</FormLabel>
-								</FormItem>
-								<FormItem className='flex space-x-4 items-center justify-center'>
-									<RadioGroupItem value='DELETE' />
-									<FormLabel className='leading-none !mt-0'>Delete</FormLabel>
-								</FormItem>
-							</RadioGroup>
-						</div>
-
-						{/* ID Field for PUT and DELETE */}
-						{(action === 'PUT' || action === 'DELETE') && (
+						{actionType === 'PUT' && (
 							<FormField
 								control={form.control}
 								name='id'
@@ -124,7 +115,98 @@ export default function UserForm() {
 									<FormItem>
 										<FormLabel>User ID</FormLabel>
 										<FormControl>
-											<Input type='number' placeholder='Enter user ID' {...field} />
+											<Input value={field.value} disabled placeholder='User ID' />
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+						)}
+
+						{/* Username and Name Fields */}
+						<div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
+							<FormField
+								control={form.control}
+								name='username'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Username</FormLabel>
+										<FormControl>
+											<Input placeholder='Username' {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name='name'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Name</FormLabel>
+										<FormControl>
+											<Input placeholder='Full Name' {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+						</div>
+
+						{/* Email and Phone Fields */}
+						<div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
+							<FormField
+								control={form.control}
+								name='email'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Email</FormLabel>
+										<FormControl>
+											<Input type='email' placeholder='Email Address' {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name='phone'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Phone</FormLabel>
+										<FormControl>
+											<Input placeholder='Phone Number' {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+						</div>
+
+						{/* Role Field */}
+						<FormField
+							control={form.control}
+							name='role'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Role</FormLabel>
+									<FormControl>
+										<Input placeholder='Role (e.g., Admin, User)' {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
+						{/* Password Field (Only for POST Action) */}
+						{actionType === 'POST' && (
+							<FormField
+								control={form.control}
+								name='password'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Password</FormLabel>
+										<FormControl>
+											<Input type='password' placeholder='Password' {...field} />
 										</FormControl>
 										<FormMessage />
 									</FormItem>
@@ -132,101 +214,7 @@ export default function UserForm() {
 							/>
 						)}
 
-						{/* Other fields for POST and PUT */}
-						{(action === 'POST' || action === 'PUT') && (
-							<>
-								<div className='grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6'>
-									<FormField
-										control={form.control}
-										name='name'
-										render={({ field }) => (
-											<FormItem>
-												<FormLabel>Name</FormLabel>
-												<FormControl>
-													<Input placeholder='Enter your name' {...field} />
-												</FormControl>
-												<FormMessage />
-											</FormItem>
-										)}
-									/>
-									<FormField
-										control={form.control}
-										name='username'
-										render={({ field }) => (
-											<FormItem>
-												<FormLabel>Username</FormLabel>
-												<FormControl>
-													<Input placeholder='Enter username' {...field} />
-												</FormControl>
-												<FormMessage />
-											</FormItem>
-										)}
-									/>
-								</div>
-
-								<div className='grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6'>
-									<FormField
-										control={form.control}
-										name='email'
-										render={({ field }) => (
-											<FormItem>
-												<FormLabel>Email</FormLabel>
-												<FormControl>
-													<Input type='email' placeholder='Enter your email' {...field} />
-												</FormControl>
-												<FormMessage />
-											</FormItem>
-										)}
-									/>
-									<FormField
-										control={form.control}
-										name='phone'
-										render={({ field }) => (
-											<FormItem>
-												<FormLabel>Phone</FormLabel>
-												<FormControl>
-													<Input placeholder='Enter your phone number' {...field} />
-												</FormControl>
-												<FormMessage />
-											</FormItem>
-										)}
-									/>
-								</div>
-
-								<div className='grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6'>
-									<FormField
-										control={form.control}
-										name='role'
-										render={({ field }) => (
-											<FormItem>
-												<FormLabel>Role</FormLabel>
-												<FormControl>
-													<Input placeholder='Enter role (admin/user)' {...field} />
-												</FormControl>
-												<FormMessage />
-											</FormItem>
-										)}
-									/>
-									{action === 'POST' && (
-										<FormField
-											control={form.control}
-											name='password'
-											render={({ field }) => (
-												<FormItem>
-													<FormLabel>Password</FormLabel>
-													<FormControl>
-														<Input type='password' placeholder='Enter password' {...field} />
-													</FormControl>
-													<FormMessage />
-												</FormItem>
-											)}
-										/>
-									)}
-								</div>
-							</>
-						)}
-
-						<Button type='submit'>{action} User</Button>
+						<Button type='submit'>{actionType === 'POST' ? 'Add User' : 'Update User'}</Button>
 					</form>
 				</Form>
 			</CardContent>
