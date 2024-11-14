071182d113d5d1f7c240fdeb420a3d272dccd13e
Halim <halimputra0701@gmail.com>
2024-11-14 13:01:23 +0700

    refactor(layout): remove metadata

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,4 @@
-import type { Metadata } from 'next';
 import AppSidebar from '@/components/layout/app-sidebar';
-
-export const metadata: Metadata = {
-	title: 'BeresIn Dashboard',
-	description: 'Basic dashboard with Next.js and Shadcn',
-};
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
 	return (
