c94bfc07a412945c3f61f32bae3cf6e5b851bc80
Halim <halimputra0701@gmail.com>
2024-11-14 12:55:24 +0700

    refactor(page): add metadata, add session

--- src/app/dashboard/user/[userId]/page.tsx
+++ src/app/dashboard/user/[userId]/page.tsx
@@ -1,9 +1,21 @@
-import UserViewPage from '../_components/user-view-page';
+// src/app/dashboard/user/[userId]/page.tsx
+import { getServerSession } from 'next-auth';
+import authConfig from '@/utils/auth.config';
+import { redirect } from 'next/navigation';
+import UserForm from '../_components/user-form';
+import { Metadata } from 'next';
 
-export const metadata = {
-  title: 'Dashboard : User View'
+export const metadata: Metadata = {
+	title: 'User : Edit',
+	description: 'Edit BeresIn User',
 };
 
-export default function Page() {
-  return <UserViewPage />;
+export default async function UserIdPage({ params }: { params: { userId: string } }) {
+	const session = await getServerSession(authConfig);
+
+	if (!session) {
+		return redirect('/');
+	}
+
+	return <UserForm userId={params.userId} session={session} actionType="PUT" />;
 }
