e6c66fb08862b2d20fa93936c9b4ae752b9d080e
Halim <halimputra0701@gmail.com>
2024-11-17 23:25:09 +0700

    refactor(component): add paramps into type

--- src/app/dashboard/service/_components/service-view-page.tsx
+++ src/app/dashboard/service/_components/service-view-page.tsx
@@ -1,16 +1,20 @@
 // src/app/dashboard/service/_components/service-view-page.tsx
 import { getServerSession } from 'next-auth';
 import { notFound } from 'next/navigation';
+import { getServiceById } from '@/actions/service/get-services';
 import { Service } from '@/types/service';
 import authConfig from '@/utils/auth.config';
 import ServiceForm from './service-form';
-import { getServiceById } from '@/actions/service/get-services';
 
 type TServiceViewPageProps = {
-	serviceId: string;
+	params: {
+		serviceId: string;
+	};
 };
 
-export default async function ServiceViewPage({ serviceId }: TServiceViewPageProps) {
+export default async function ServiceViewPage({ params }: TServiceViewPageProps) {
+	const { serviceId } = params;
+
 	const session = await getServerSession(authConfig);
 
 	if (!session || !session.accessToken) {
