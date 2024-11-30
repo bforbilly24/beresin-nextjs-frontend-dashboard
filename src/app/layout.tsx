ae618668808923b7ef781286ac8fa3a233a9d15d
Halim <halimputra0701@gmail.com>
2024-11-30 22:17:41 +0700

    refactor(layout): update content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
 import type { Metadata } from 'next';
+import { getServerSession } from 'next-auth';
 import { Lato } from 'next/font/google';
 import NextTopLoader from 'nextjs-toploader';
 import Providers from '@/components/layout/providers';
+import { NotReadyResponsive } from '@/components/miscellaneous/not-ready-responsive';
 import { Toaster } from '@/components/ui/sonner';
-import './globals.css';
-import { getServerSession } from 'next-auth';
 import authConfig from '@/utils/auth.config';
+import './globals.css';
+import { ProgressBarProvider } from '@/components/providers/progress-bar-provider';
 
-export const metadata: Metadata =  {
+export const metadata: Metadata = {
 	title: {
 		template: 'BeresIn | %s ',
 		default: 'BeresIn',
@@ -31,13 +33,17 @@ const lato = Lato({
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
 	const session = await getServerSession(authConfig);
+
 	return (
 		<html lang='en' className={`${lato.className}`} suppressHydrationWarning={true}>
 			<body className='overflow-hidden'>
 				<NextTopLoader showSpinner={false} />
 				<Providers session={session}>
-					<Toaster />
-					{children}
+					<ProgressBarProvider>
+						<Toaster />
+						<main className='xl:flex h-screen hidden'>{children}</main>
+					</ProgressBarProvider>
+					<NotReadyResponsive />
 				</Providers>
 			</body>
 		</html>
