7f218a39933abf234625ac49186210645ae2ac8f
Halim <halimputra0701@gmail.com>
2024-11-30 18:40:48 +0700

    refactor(page): change icon filter, remove unused comment

--- src/app/dashboard/service/_components/service-filter-category.tsx
+++ src/app/dashboard/service/_components/service-filter-category.tsx
@@ -1,5 +1,5 @@
-import { CheckIcon } from '@radix-ui/react-icons';
-import { ListFilterIcon } from 'lucide-react';
+import { Cross2Icon } from '@radix-ui/react-icons';
+import { SlidersHorizontalIcon } from 'lucide-react';
 import { useState } from 'react';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
@@ -7,7 +7,6 @@ import { Checkbox } from '@/components/ui/checkbox';
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from '@/components/ui/command';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { Separator } from '@/components/ui/separator';
-import { cn } from '@/lib/utils';
 
 interface FilterCategoryProps {
 	selectedCategory: string | null;
@@ -19,66 +18,75 @@ const ServiceFilterCategory: React.FC<FilterCategoryProps> = ({ selectedCategory
 	const [filterValue, setFilterValue] = useState<string | null>(null);
 
 	const handleCategoryChange = (categoryId: string | null) => {
-		onCategoryChange(categoryId); // Set category to the selected category
+		onCategoryChange(categoryId);
 	};
 
 	const resetFilter = () => {
-		onCategoryChange(null); // Clear the selected category
-		setFilterValue(null); // Reset filter value
+		onCategoryChange(null); 
+		setFilterValue(null);
 	};
 
 	return (
-		<Popover>
-			<PopoverTrigger asChild>
-				<Button variant='outline' size='sm' className='ml-auto hidden h-8 lg:flex'>
-					<ListFilterIcon className='w-4 h-4 mr-1' />
-					Categories
-					{selectedCategory && (
-						<>
-							<Separator orientation='vertical' className='mx-2 h-4' />
-							<Badge variant='secondary' className='rounded-sm px-1 font-normal'>
-								{categories.find((cat) => cat.id === selectedCategory)?.name_of_category}
-							</Badge>
-						</>
-					)}
-				</Button>
-			</PopoverTrigger>
-
-			<PopoverContent className='w-[200px] p-0' align='start'>
-				<Command>
-					<CommandInput placeholder='Filter categories...' value={filterValue} onValueChange={setFilterValue} />
-					<CommandList>
-						{categories.length === 0 ? (
-							<CommandEmpty>No categories found</CommandEmpty>
-						) : (
-							<CommandGroup>
-								{categories.map((category) => (
-									<CommandItem key={category.id} onSelect={() => handleCategoryChange(category.id)}>
-										<div className='flex items-center'>
-											<Checkbox id={category.id} checked={selectedCategory === category.id} onCheckedChange={() => handleCategoryChange(category.id)} />
-											<label htmlFor={category.id} className='ml-2'>
-												{category.name_of_category}
-											</label>
-										</div>
-									</CommandItem>
-								))}
-							</CommandGroup>
-						)}
-
+		<div className='flex flex-row gap-x-2 items-center justify-start'>
+			<Popover>
+				<PopoverTrigger asChild>
+					<Button variant='outline' size='sm' className='ml-auto hidden h-8 lg:flex'>
+						<SlidersHorizontalIcon className='w-4 h-4 mr-1' />
+						Categories
 						{selectedCategory && (
 							<>
-								<CommandSeparator />
-								<CommandGroup>
-									<CommandItem onSelect={resetFilter} className='justify-center text-center'>
-										Clear filter
-									</CommandItem>
-								</CommandGroup>
+								<Separator orientation='vertical' className='mx-2 h-4' />
+								<Badge variant='secondary' className='rounded-sm px-1 font-normal'>
+									{categories.find((cat) => cat.id === selectedCategory)?.name_of_category}
+								</Badge>
 							</>
 						)}
-					</CommandList>
-				</Command>
-			</PopoverContent>
-		</Popover>
+					</Button>
+				</PopoverTrigger>
+
+				<PopoverContent className='w-[200px] p-0' align='start'>
+					<Command>
+						<CommandInput placeholder='Filter categories...' value={filterValue} onValueChange={setFilterValue} />
+						<CommandList>
+							{categories.length === 0 ? (
+								<CommandEmpty>No categories found</CommandEmpty>
+							) : (
+								<CommandGroup>
+									{categories.map((category) => (
+										<CommandItem key={category.id} onSelect={() => handleCategoryChange(category.id)}>
+											<div className='flex items-center'>
+												<Checkbox id={category.id} checked={selectedCategory === category.id} onCheckedChange={() => handleCategoryChange(category.id)} />
+												<label htmlFor={category.id} className='ml-2'>
+													{category.name_of_category}
+												</label>
+											</div>
+										</CommandItem>
+									))}
+								</CommandGroup>
+							)}
+
+							{selectedCategory && (
+								<>
+									<CommandSeparator />
+									<CommandGroup>
+										<CommandItem onSelect={resetFilter} className='justify-center text-center'>
+											Clear filter
+										</CommandItem>
+									</CommandGroup>
+								</>
+							)}
+						</CommandList>
+					</Command>
+				</PopoverContent>
+			</Popover>
+
+			{selectedCategory && (
+				<Button size='sm' onClick={resetFilter}>
+					<Cross2Icon className='mr-1 h-4 w-4' />
+					Clear Filters
+				</Button>
+			)}
+		</div>
 	);
 };
 
