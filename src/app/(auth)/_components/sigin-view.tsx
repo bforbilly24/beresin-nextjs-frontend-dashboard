d74fe51c427a624d61f05070ab49b4f9b3c9653f
Halim <halimputra0701@gmail.com>
2024-12-01 04:08:39 +0700

    style(page): update logo brand login, update content

--- src/app/(auth)/_components/sigin-view.tsx
+++ src/app/(auth)/_components/sigin-view.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import Image from 'next/image';
 import Link from 'next/link';
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
@@ -18,15 +19,15 @@ export default function SignInViewPage() {
 			<div className='relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r'>
 				<div className='absolute inset-0 bg-zinc-900' />
 				<div className='relative z-20 flex items-center text-lg font-medium'>
-					<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' className='mr-2 h-6 w-6'>
-						<path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
-					</svg>
-					Logo
+					<Image src='/beresin.svg' alt='beresin-logo' width='100' height='100' />
 				</div>
 				<div className='relative z-20 mt-auto'>
 					<blockquote className='space-y-2'>
-						<p className='text-lg'>&ldquo;This library has saved me countless hours of work and helped me deliver stunning designs to my clients faster than ever before.&rdquo;</p>
-						<footer className='text-sm'>Sofia Davis</footer>
+						<p className='text-base'>
+							&ldquo;Solusi Jasa Kampus, Dari Mahasiswa untuk Mahasiswa! Aplikasi marketplace berbasis mobile yang memudahkan mahasiswa menemukan dan menawarkan berbagai jasa di kampus dengan mudah, cepat, dan terpercaya. Temukan solusi terbaik dari sesama mahasiswa untuk kebutuhan
+							akademik maupun non-akademik dalam satu platform!&rdquo;
+						</p>
+						<footer className='text-sm'>BeresIn</footer>
 					</blockquote>
 				</div>
 			</div>
