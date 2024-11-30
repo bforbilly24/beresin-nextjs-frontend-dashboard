1a78b97b78939e59fde2904bf5310099c6ba1bac
Halim <halimputra0701@gmail.com>
2024-11-30 18:59:14 +0700

    refactor(component): update `checkbox` shadcn/ui

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -7,8 +7,8 @@ import { CheckIcon } from '@radix-ui/react-icons';
 import { cn } from '@/lib/utils';
 
 const Checkbox = React.forwardRef<
-  React.ElementRef<typeof CheckboxPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+  React.ElementRef<typeof CheckboxPrimitive.Root>, 
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> 
 >(({ className, ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
@@ -16,7 +16,7 @@ const Checkbox = React.forwardRef<
       'peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
       className
     )}
-    {...props}
+    {...props} 
   >
     <CheckboxPrimitive.Indicator
       className={cn('flex items-center justify-center text-current')}
@@ -25,6 +25,7 @@ const Checkbox = React.forwardRef<
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ));
+
 Checkbox.displayName = CheckboxPrimitive.Root.displayName;
 
 export { Checkbox };
