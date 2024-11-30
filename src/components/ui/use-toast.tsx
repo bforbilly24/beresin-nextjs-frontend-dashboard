f532091600df8c29fc41ab5ef443af4d5edd48a4
Halim <halimputra0701@gmail.com>
2024-12-01 03:31:37 +0700

    refactor(component): add type `use-toast` shadcn/ui

--- src/components/ui/use-toast.tsx
+++ src/components/ui/use-toast.tsx
@@ -118,23 +118,29 @@ function toast(props: Omit<Toast, 'id' | 'open' | 'onOpenChange'>): {
 	const id = genId();
 
 	const update = (props: Omit<Toast, 'id' | 'open' | 'onOpenChange'>) =>
-		dispatch({
-			type: 'UPDATE_TOAST',
-			toast: { ...props, id },
-		});
-	const dismiss = () => dispatch({ type: 'DISMISS_TOAST', toastId: id });
-
-	dispatch({
-		type: 'ADD_TOAST',
-		toast: {
-			...props,
-			id,
-			open: true,
-			onOpenChange: (open: boolean) => {
-				if (!open) dismiss();
-			},
-		},
-	});
+        dispatch({
+          type: 'UPDATE_TOAST',
+          toast: {
+            ...props,
+            id,
+            open: false, 
+          },
+        });
+      
+      const dismiss = () => dispatch({ type: 'DISMISS_TOAST', toastId: id });
+      
+      dispatch({
+        type: 'ADD_TOAST',
+        toast: {
+          ...props,
+          id,
+          open: true, // Ensure 'open' is set to true when adding a new toast
+          onOpenChange: (open: boolean) => {
+            if (!open) dismiss();
+          },
+        },
+      });
+      
 
 	return {
 		id,
