b2a1824d50f24cbf174aec92160b5550419a1428
Halim <halimputra0701@gmail.com>
2024-11-30 18:56:10 +0700

    feat(type): add types/service

--- src/types/service.d.ts
+++ src/types/service.d.ts
@@ -1,4 +1,3 @@
-
 export type ServiceStatus = 'accept' | 'decline' | 'pending';
 
 export interface ServiceImage {
@@ -10,12 +9,19 @@ export interface Service {
 	created_at: string;
 	updated_at: string;
 	user_id: number;
-	isSubscription: boolean;
 	name_of_service: string;
 	category_id: number;
+	category_name?: string;
 	description: string;
 	status: ServiceStatus;
+	min_price: number;
+	max_price: number;
+	like_count: number;
+	bookmark_count: number;
 	images: string[];
+	subscription: Subscription;
+	isSubscription: boolean;
+    expired_at: string;
 }
 
 export interface ServiceResponse {
