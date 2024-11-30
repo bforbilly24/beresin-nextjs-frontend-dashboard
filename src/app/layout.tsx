b64d2fbd6733edfec73e4f6088e8a4aad9dfe0b5
Halim <halimputra0701@gmail.com>
2024-12-01 04:21:16 +0700

    refactor(layout): add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from 'next';
 import { getServerSession } from 'next-auth';
 import { Lato } from 'next/font/google';
+import Head from 'next/head';
 import NextTopLoader from 'nextjs-toploader';
 import Providers from '@/components/layout/providers';
 import { NotReadyResponsive } from '@/components/miscellaneous/not-ready-responsive';
+import { ProgressBarProvider } from '@/components/providers/progress-bar-provider';
 import { Toaster } from '@/components/ui/sonner';
 import authConfig from '@/utils/auth.config';
 import './globals.css';
-import { ProgressBarProvider } from '@/components/providers/progress-bar-provider';
 
 export const metadata: Metadata = {
 	title: {
@@ -36,6 +37,9 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 
 	return (
 		<html lang='en' className={`${lato.className}`} suppressHydrationWarning={true}>
+			<Head>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 			<body className='overflow-hidden'>
 				<NextTopLoader showSpinner={false} />
 				<Providers session={session}>
